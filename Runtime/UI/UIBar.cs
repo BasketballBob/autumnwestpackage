835dcac6679a704c706b2c184dfdd22726927480
Autumn West <45017247+BasketballBob@users.noreply.github.com>
2025-09-02 14:30:25 -0700

    Update UIBar.cs

--- Runtime/UI/UIBar.cs
+++ Runtime/UI/UIBar.cs
@@ -10,7 +10,8 @@ namespace AWP
     public class UIBar : AWUI
     {
         [OnValueChanged("SyncVisuals")]
-        public float Delta = 1;
+        [SerializeField]
+        private float _delta = 1;
         [SerializeField]
         private RectTransform _rectTrans;
         [SerializeField]
@@ -20,6 +21,8 @@ namespace AWP
 
         [SerializeReference]
         private DeltaVariable _optionalVariable;
+
+        public float Delta => _delta;
 
         private void Reset()
         {
@@ -41,17 +44,23 @@ namespace AWP
             
         }
 
+        public void SetDelta(float delta)
+        {
+            _delta = delta;
+            SyncVisuals();
+        }
+
         private void SyncVisuals()
         {
-            Delta = Mathf.Clamp01(Delta);
+            _delta = Mathf.Clamp01(_delta);
 
-            _mask.rectTransform.sizeDelta = _mask.rectTransform.sizeDelta.SetX(-_rectTrans.sizeDelta.x * (1 - Delta));
+            _mask.rectTransform.sizeDelta = _mask.rectTransform.sizeDelta.SetX(-_rectTrans.sizeDelta.x * (1 - _delta));
             _image.rectTransform.position = _rectTrans.position;
         }
 
         private void SyncVariable()
         {
-            Delta = _optionalVariable.Delta;
+            _delta = _optionalVariable.Delta;
             SyncVisuals();
         }
     }
