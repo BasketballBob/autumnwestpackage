f6889a2d80a06bec6b36a1f1c5e4c2695ab403d5
Autumn West <45017247+BasketballBob@users.noreply.github.com>
2025-09-17 18:00:04 -0700

    Update IgnoreCollision2D.cs

--- Runtime/Physics2D/IgnoreCollision2D.cs
+++ Runtime/Physics2D/IgnoreCollision2D.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using AWP;
 using Sirenix.Utilities;
 using UnityEngine;
 
 public class IgnoreCollision2D : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D _rb;
     [SerializeField]
     private Collider2D _callingCollider;
 
@@ -14,11 +17,18 @@ public class IgnoreCollision2D : MonoBehaviour
     private void OnEnable()
     {
         IgnoreCollisions(_callingCollider);
+        IgnoreCollisions(_rb);
     }
 
     public void IgnoreCollisions(Collider2D col)
     {
         IgnoreCollisions(col, _ignoredColliders);
+    }
+
+    public void IgnoreCollisions(Rigidbody2D rb)
+    {
+        if (rb == null) return;
+        _ignoredColliders.ForEach(x => AWPhysics2D.IgnoreRigidbodyColliderCollision(rb, x));
     }
 
     public static void IgnoreCollisions(Collider2D col, List<Collider2D> ignoredColliders)
