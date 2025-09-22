240e8d5839de0ad6de0b4a7a2ffb545f19ed4d59
Autumn West <45017247+BasketballBob@users.noreply.github.com>
2025-09-22 14:55:32 -0700

    Added BoolVariableEvent

---
+++ Runtime/Utility/BoolVariableEvents.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AWP
+{
+    public class BoolVariableEvents : MonoBehaviour
+    {
+        [SerializeField]
+        private BoolVariable _boolVariable;
+        [SerializeField]
+        private UnityEvent _onSetTrue;
+        [SerializeField]
+        private UnityEvent _onSetFalse;
+
+        private void OnEnable()
+        {
+            _boolVariable.OnValueChanged += OnValueChanged;
+        }
+
+        private void OnDisable()
+        {
+            _boolVariable.OnValueChanged -= OnValueChanged;
+        }
+
+        private void OnValueChanged()
+        {
+            if (_boolVariable.RuntimeValue) _onSetTrue.Invoke();
+            else _onSetFalse.Invoke();
+
+            Debug.Log($"BOOL CHANGED {_boolVariable.RuntimeValue}");
+        }
+    }
+}
