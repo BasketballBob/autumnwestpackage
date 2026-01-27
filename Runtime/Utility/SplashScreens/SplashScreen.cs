cd57b4afe927d6469eb5aa06fe24663932bbacdc
Autumn West <45017247+BasketballBob@users.noreply.github.com>
2026-01-27 14:14:36 -0800

    Update SplashScreen.cs

--- Runtime/Utility/SplashScreens/SplashScreen.cs
+++ Runtime/Utility/SplashScreens/SplashScreen.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
 
 namespace AWP
 {
@@ -13,12 +15,23 @@ namespace AWP
         [SerializeField]
         private RectTransform _rect;
 
+        [SerializeField]
+        private EventReference _displaySFX;
+
+        private EventInstance _displayInstance;
+
         public Canvas Canvas => _canvas;
         public RectTransform Rect => _rect;
+
+        private void OnDestroy()
+        {
+            StopDisplaySFX();
+        }
 
         public IEnumerator Display()
         {
             _anim.Play("Display");
+            TryPlayDisplaySFX();
             yield return WaitForDisplayToFinish();
         }
 
@@ -32,5 +45,20 @@ namespace AWP
             if (_anim == null) return true;
             return _anim.AnimationIsFinished();
         }
+
+        #region SFX
+        private void TryPlayDisplaySFX()
+        {
+            if (_displaySFX.IsNull) return;
+
+            _displayInstance = AWGameManager.AudioManager.CreateInstance(_displaySFX);
+            _displayInstance.start();
+        }
+
+        private void StopDisplaySFX()
+        {
+            _displayInstance.DisposeOfSelf();
+        }
+        #endregion
     }
 }
